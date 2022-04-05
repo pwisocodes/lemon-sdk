ed6520e1377e0688329aa4c09787f921df2cf516
fapfaff <fabian-pfaff@outlook.de>
2022-04-05 20:31:09 +0200

    Added helper to convert from eur to amount

--- lemon/common/helpers.py
+++ lemon/common/helpers.py
@@ -2,16 +2,19 @@ from datetime import datetime
 
 
 class Singleton(type):
-    """ Metaclass Singleton
-    """
+    """Metaclass Singleton"""
+
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(
-                Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
 def datetime_to_unixtimestamp(today: datetime, ddays: int, dhour: int, dmin: int):
     return (today.timedelta(days=ddays, hours=dhour, minutes=dmin)).strftime("%s")
+
+
+def eur_to_amount(amount: float):
+    return int(amount * 10000)
